4d3fe018b861bed86cce568f36da7407d3377497
cmjt <cmjonestodd@gmail.com>
2023-11-20 13:29:16 +1300

    adding snowman game for spatial stats words

---
+++ snowman/README.md
@@ -0,0 +1,9 @@
+## Summary
+
+[snowman](https://statbiscuit.github.io/mini_games/puzzle/snowman.html): guess the word before the snowman melts.
+
+## Notes
+
+Modify the `word` and `hint` elements in `word-list.js` to customize. Change the images in `images` to modify the tally imagery.
+
+The base code is modified from [Hangman Game](https://www.codingnepalweb.com/build-hangman-game-html-javascript/)
---
+++ snowman/word-list.js
@@ -0,0 +1,54 @@
+const wordList = [
+    {
+        word: "pattern",
+        hint: "Realisation of a point process: point ___."
+    },
+    {
+        word: "spatstat",
+        hint: "R package for spatial statistics authored by Adrian Baddeley, Rolf Turner and Ege Rubak."
+    },
+    {
+        word: "cartesian",
+        hint: "A coordinate reference systems."
+    },
+     {
+        word: "gaussian",
+        hint: "log-___ Cox process."
+    },
+    {
+        word: "kriging",
+        hint: "A method of spatial interpolation."
+    },
+    {
+        word: "interploation",
+        hint: "Spatial ___: estimating values for spatial locations where they have not been observed."
+    },
+    {
+        word: "gaussian",
+        hint: "log-___ Cox process."
+    },
+    {
+        word: "neyman",
+        hint: "___-Scott process."
+    },
+    {
+        word: "covariance",
+        hint: "Matérn ___ function."
+    },
+    {
+        word: "stelfi",
+        hint: "R package for Hawkes and Log-Gaussian Cox Point Processes."
+    },
+    {
+        word: "hawkes",
+        hint: "A self-exciting temporal point process."
+    },
+    {
+        word: "stochastic",
+        hint: "___  partial differential equation."
+    },
+    {
+        word: "sasquatch",
+        hint: "A large and hairy human-like mythical creature believed to exist in the northwestern US and western Canada."
+    },
+];
