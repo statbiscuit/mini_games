a40c5c46b740c26131b7efc69e9ae3c97ba165bb
Cris Escandor <cyrescandor241@gmail.com>
2025-02-16 22:18:57 +1300

    separating room const into another js

--- zork/app.js
+++ zork/app.js
@@ -1,90 +1,85 @@
-// Select input and output elements
+/********************************************
+               GAME SETUP
+ ********************************************/
+
 const inputEl = document.getElementById('input');
 const outputEl = document.getElementById('output');
 
-// Auto-focus input on load
 inputEl.focus();
-
-// Load room data from external file (rooms.js)
-import { rooms } from './rooms.js';
-
-// Track the current room
-let currentRoom = 'start';
-
-// Command Constants
-const COMMANDS = {
-    LOOK: 'look',
-    TORCH: 'torch',
-    COMPANION: 'CAI',
-    PLAY: 'PLAY',
-    MOVE_NORTH: 'go north',
-    MOVE_SOUTH: 'go south',
-    MOVE_EAST: 'go east',
-    MOVE_WEST: 'go west'
-};
-
-// Handle user input
 inputEl.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
-        const command = inputEl.value.trim().toLowerCase();
-        processCommand(command);
+        const command = inputEl.value.trim(); // Trim whitespace
+        handleCommand(command); // Process the command
         inputEl.value = ''; // Clear input field
     }
 });
 
-// Process player commands
-function processCommand(command) {
+// Track the player's current room
+let currentRoom = 'start';
+
+// import rooms
+import { rooms } from './rooms.js';
+
+
+/********************************************
+             COMMAND HANDLING
+ ********************************************/
+
+function handleCommand(command) {
     let output = '';
 
     switch (command) {
-        case COMMANDS.PLAY:
-            output = rooms[currentRoom].play || "You can't play here.";
+        case 'PLAY':
+            output = rooms[currentRoom].play;
             break;
 
-        case COMMANDS.TORCH:
-            output = rooms[currentRoom].help || "Nothing happens.";
+        case 'torch':
+            output = rooms[currentRoom].help;
             break;
 
-        case COMMANDS.COMPANION:
-            output = rooms[currentRoom].companion || "Your companion is silent.";
+        case 'CAI':
+            output = rooms[currentRoom].companion;
             break;
 
-        case COMMANDS.LOOK:
+        case 'look':
             output = rooms[currentRoom].description;
             break;
 
+        case 'go north':
+        case 'go south':
+        case 'go east':
+        case 'go west':
+            output = handleMovement(command);
+            break;
+
         default:
-            // Handle movement dynamically
-            if (command.startsWith('go ')) {
-                output = movePlayer(command.split(' ')[1]);
-            } else {
-                output = `I don't understand "${command}". Try another command.`;
-            }
+            output = `I don't understand "<strong>${command}</strong>"`;
     }
 
     updateOutput(command, output);
 }
 
-// Move player to a new room
-function movePlayer(direction) {
+/********************************************
+             MOVEMENT HANDLING
+ ********************************************/
+
+function handleMovement(command) {
+    const direction = command.split(' ')[1]; // Extract direction
     if (rooms[currentRoom].exits[direction]) {
         currentRoom = rooms[currentRoom].exits[direction];
         return rooms[currentRoom].description;
+    } else {
+        return `You can't go that way.`;
     }
-    return "You can't go that way.";
 }
 
-// Update game output
+/********************************************
+             OUTPUT HANDLING
+ ********************************************/
+
 function updateOutput(command, output) {
-    outputEl.innerHTML += `
-        <div class="prompt">></div>
-        <div>${command}</div>
-        <div>${output}</div>
-    `;
+    outputEl.innerHTML += `<div class="prompt">></div>${command}<br>${output}`;
 }
 
-// Initialize the game with a welcome message
-outputEl.innerHTML += `
-    <div><h3>Nau mai, haere mai. Welcome to zoRk!</h3></div>
-    <div>${rooms[currentRoom].description}</div>
-`;
+// Display initial game message
+outputEl.innerHTML += `<div><h3>Nau mai, haere mai.<br>Welcome to zoRk!</h3><br>${rooms[currentRoom].description}</div>`;
--- zork/rooms.js
+++ zork/rooms.js
@@ -1,58 +1,28 @@
-export const rooms = {
-    start: {
-        description: "You are in a dark room, alone, apart from your faithful companion CAI.",
-        companion: `
-           __
-      (___()'`;
-      /,    /`;
-      \\"--\\ AWWOOF Looks like there's a door over there.
-      Do you have any treats? No? Just a torch... AWWOOF`,
-        help: "There is a door to your north, perhaps you should go north?",
-        play: "There is no one to play with here.",
-        exits: { north: 'hallway' }
-    },
+// rooms.js - Defines all game rooms and their properties
 
-    hallway: {
-        description: "You are in a long hallway and your torch flickers out.",
-        companion: `
-           __
-      (___()'`;
-      /,    /`;
-      \\"--\\ AWWOOF Which way now? AWWOOF`,
-        help: "Your torch no longer works.",
-        play: "There is no one to play with here.",
-        exits: { south: 'start', east: 'treasureRoom' }
-    },
-
-    treasureRoom: {
-        description: `
-Whakamihi!!     
-     ^    ^
-    / \\  //\\
- |\\___/|      /   \\/\\/  .\\
- /O  O  \\__  /    \\/\\/  | \\ \\
-/     /  \\/_/    \\/\\/   |  \\  \\
-@___@'    \\/_   \\/\\/    |   \\   \\ 
-   |       \\/_ \\/\\/     |    \\    \\ 
-   |        \\/\\/\\/      |     \\     \\ 
-  _|_ /   )  \\/\\/       |      \\     _\\
- '/,_ _ _/  ( ; -.    |    _ _\\.-~        .-~~~^-.
- ,-{        _      `-.|.-~-.           .~         `.
-  '/\\      /                 ~-. _ .-~      .-~^-.  \\
-     `.   {            }                   /      \\  \\
-   .----~-.\\        \\-'                 .~         \\  `. \\^-.
-  /\\/\\/\\.----..>    c   \\             _ -~             `.  ^-`   ^-_
-    /\\/\\/-._ _ _ _ _ _ _}^ - - - - ~                     ~--,   .-~
-                                                          /.-'
-
-You found Andarna!!`,
-        companion: `
-           __
-      (___()'`;
-      /,    /`;
-      \\"--\\ AWWOOF AWWOOF Does she want to PLAY with us? AWWOOF`,
-        help: "Your torch no longer works.",
-        play: "Dragons do not play. We are far too regal to mix with mere mortals.",
-        exits: { west: 'hallway' }
-    }
+const rooms = {
+  start: {
+      description: " <span style='color: blue'>You are in a dark room, alone, apart from your faithful companion </span> CAI",
+      companion: "<span style='color: blue'>           __\r\n      (___()\'`;\r\n      \/,    \/`\r\n      \\\\\"--\\\\ AWWOOF Looks like there's a door over there.\nDo you have any treats? No? Just a </span> torch<span style='color: blue'>... AWWOOF </span>",
+      help: "<span style='color: blue'>There is a door to your north, perhaps you should </span> go north?",
+      play: "<span style='color: blue'>There is no one to </span> play <span style='color: blue'>with here</span>",
+      exits: {north: 'hallway'},
+  },
+  hallway: {
+      description: "<span style='color: blue'>You are in a long hallway and your</span> torch<span style='color: blue'> flickers out</span>",
+      companion: "<span style='color: blue'>           __\r\n      (___()\'`;\r\n      \/,    \/`\r\n      \\\\\"--\\\\ AWWOOF Which way now? AWWOOF</span>",
+      help: "<span style='color: blue'>Your torch no longer works</span>",
+      play: "<span style='color: blue'>There is no one to play with here</span>",
+      exits: {south: 'start', east: 'treasureRoom'},
+  },
+  treasureRoom: {
+      description: "<span style='color: blue'>Whakamihi!!     ^    ^\r\n               \/ \\  \/\/\\\r\n |\\___\/|      \/   \\\/\/  .\\\r\n \/O  O  \\__  \/    \/\/  | \\ \\\r\n\/     \/  \\\/_\/    \/\/   |  \\  \\\r\n@___@\'    \\\/_   \/\/    |   \\   \\ \r\n   |       \\\/_ \/\/     |    \\    \\ \r\n   |        \\\/\/\/      |     \\     \\ \r\n  _|_ \/   )  \/\/       |      \\     _\\\r\n \'\/,_ _ _\/  ( ; -.    |    _ _\\.-~        .-~~~^-.\r\n ,-{        _      `-.|.-~-.           .~         `.\r\n  \'\/\\      \/                 ~-. _ .-~      .-~^-.  \\\r\n     `.   {            }                   \/      \\  \\\r\n   .----~-.\\        \\-\'                 .~         \\  `. \\^-.\r\n  \/\/\/.----..>    c   \\             _ -~             `.  ^-`   ^-_\r\n    \/\/\/-._ _ _ _ _ _ _}^ - - - - ~                     ~--,   .-~\r\n                                                          \/.-\'\r\n\r\n You found Andarna!!",
+      companion: "<span style='color: blue'>           __\r\n      (___()\'`;\r\n      \/,    \/`\r\n      \\\\\"--\\\\ AWWOOF AWWOOF Does she want to</span> PLAY <span style='color: blue'>with us? AWWOOF</span>",
+      help: "<span style='color: blue'>Your torch no longer works</span>",
+      play: "<span style='color: blue'>Dragons do not play. We are far too regal to mix with mere mortals</span>",
+      exits: {west: 'hallway'},
+  }
 };
+
+// Export rooms object so it can be used in app.js
+export { rooms };
